73b8c49486b9b9702de7ba97e40fdabef4362859
vo1dstarr <void@vo1dstarr.com>
2026-01-07 17:57:07 -0500

    Fill in Template with Plugin and Repo info

--- main.py
+++ main.py
@@ -13,15 +13,15 @@ class PluginTemplate(PluginBase):
         self.simple_action_holder = ActionHolder(
             plugin_base = self,
             action_base = SimpleAction,
-            action_id = "dev_core447_Template::SimpleAction", # Change this to your own plugin id
+            action_id = "dev_vo1dstarr_YTMD-StreamController::SimpleAction", # Change this to your own plugin id
             action_name = "Simple Action",
         )
         self.add_action_holder(self.simple_action_holder)
 
         # Register plugin
         self.register(
-            plugin_name = "Template",
-            github_repo = "https://github.com/StreamController/PluginTemplate",
-            plugin_version = "1.0.0",
+            plugin_name = "YTMD-StreamController",
+            github_repo = "https://github.com/vo1dstarr/YTMD-StreamController",
+            plugin_version = "0.0.1",
             app_version = "1.1.1-alpha"
         )
